d0f5de315a56dd36c4e352e01264e626ba0e41e6
stradevin <stradevin@gmail.com>
2025-11-17 01:22:06 +0300

    f

--- index.js
+++ index.js
@@ -436,7 +436,7 @@ function appendBlockToAssistant(msgIndex, blockLabel, content, opts = {}) {
     const ctx = USER.getContext();
     const { eventSource, event_types, messageFormatting } = ctx;
     const S = USER.tableBaseSetting || {};
-    let cleanedContent = content.replace(/```/gi, '');
+    let cleanedContent = (blockLabel !== 'main') ? content.replace(/```/gi, '') : content;
     const msg = ctx.chat[msgIndex];
     if (!msg) return;
 
